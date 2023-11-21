2e9961550cf470a6aee6baf57801a75a4add1c99
Andrik2406 <93060312+Andrik2406@users.noreply.github.com>
2023-11-21 09:50:40 -0600

    correo
    correo

--- citas/correo.php
+++ citas/correo.php
@@ -8,7 +8,7 @@
     <title>Citas Cuam</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <script> type= "text/javascript" src="main.js"</script>
 </head>
 
 <body class="bg-dark d-flex justify-content-center align-items-center" style="height: 100vh">
@@ -18,8 +18,9 @@
         <h2 class="w-100 text-center mb-1">Confirmar cita por correo</h2>
 
         <hr style="color: #9999" />
+        
 
-        <form id="formulario" method="POST">
+        
 
             <div class="mb-3">
                 <label for="txt_correo" class="form-label">Ingrese correo electronico</label>
@@ -27,9 +28,10 @@
             </div>
             
             
+            
            
 
-            <button type="submit" class="btn btn-primary w-100 text-uppercase fw-bold">confirmar cita</button>
+            <input type="button" value="Enviar correo" onClick= "window.alert('se envio correo de confirmacion'); ">
             
 
         </form>
